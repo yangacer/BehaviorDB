614a1595b69a9025ed5c945d7b3a97d15cd8562b
yangacer <yangacer@f0538e46-0701-e011-92cc-000d616d0bd9>
2011-01-04 23:58:36 +0000

    git-svn-id: svn://gais4.cs.ccu.edu.tw/BehaviorDB/trunk@59 f0538e46-0701-e011-92cc-000d616d0bd9

---
+++ tests/put.cpp
@@ -0,0 +1,28 @@
+#include "bdb.h"
+#include <iostream>
+#include <iomanip>
+
+int main(int argc, char** argv)
+{
+	using namespace std;
+	if(argc < 2)
+		return 1;
+	
+	Config conf;
+	conf.pool_log = false;
+
+	BehaviorDB bdb(conf);
+
+	int upperbound = atoi(argv[1]);
+	char data_128[129] = "128b begin";
+
+	strcpy(data_128+128 - 7, "128b end");
+	
+	cout<<unitbuf;
+
+	for(int i=0;i<upperbound; ++i){
+		cout<<setw(8)<<setfill('0')<<hex<<bdb.put(data_128, 128)<<endl;
+	}
+
+	return 0;
+}
