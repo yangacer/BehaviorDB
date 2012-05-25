ef2eea78bbad6b9fad787c7f21aa9c7829d3776c
yang acer <yangacer@gmail.com>
2012-05-25 18:05:22 +0800

    Retab.

--- detail/v_iovec.hpp
+++ detail/v_iovec.hpp
@@ -7,39 +7,39 @@
 namespace BDB {
 
 
-	struct file_src
-	{
-		FILE *fp;
-		off_t off;
-	};
-	
-	struct no_data_src
-	{};
+  struct file_src
+  {
+    FILE *fp;
+    off_t off;
+  };
+  
+  struct no_data_src
+  {};
 
-	struct viov
-	{
-		boost::variant<file_src, char const*, no_data_src> data;
-		size_t size;
-	};
+  struct viov
+  {
+    boost::variant<file_src, char const*, no_data_src> data;
+    size_t size;
+  };
 
-	struct write_viov : public boost::static_visitor<size_t>
-	{
-		size_t 
-		operator()(char const* str);
-		
-		size_t
-		operator()(file_src & psrc);
-		
-		size_t
-		operator()(no_data_src & ndsrc);
+  struct write_viov : public boost::static_visitor<size_t>
+  {
+    size_t 
+    operator()(char const* str);
+    
+    size_t
+    operator()(file_src & psrc);
+    
+    size_t
+    operator()(no_data_src & ndsrc);
 
-		FILE* dest;
-		off_t dest_pos;
-		char *buf;
-		size_t bsize;
-		size_t size;
-	};
-	
+    FILE* dest;
+    off_t dest_pos;
+    char *buf;
+    size_t bsize;
+    size_t size;
+  };
+  
 
 } // end of namespace BDB
 
